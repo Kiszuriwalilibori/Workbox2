7f45fce68e51a663c093f9ba6b63996ce439a711
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-10-27 12:36:26 +0100

    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-//workbox.precaching.precacheAndRoute([{"revision":"54598e07758128a20943611d4c766f09","url":"css/app.css"},{"revision":"9222ac33fc1ec4c2f9bfbf304991c6e6","url":"index.html"},{"revision":"abbbfae859783538e8f1dd0c74b6ee73","url":"js/app.js"},{"revision":"104536ce72429ec1f598883183de70b7","url":"workbox-69b5a3b7.js"}]);
+workbox.precaching.precacheAndRoute([{"revision":"54598e07758128a20943611d4c766f09","url":"css/app.css"},{"revision":"9222ac33fc1ec4c2f9bfbf304991c6e6","url":"index.html"},{"revision":"abbbfae859783538e8f1dd0c74b6ee73","url":"js/app.js"},{"revision":"104536ce72429ec1f598883183de70b7","url":"workbox-69b5a3b7.js"}]);
 
 console.log('this is my custom service worker');
 
--- src-sw.js
+++ src-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-//workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 console.log('this is my custom service worker');
 
